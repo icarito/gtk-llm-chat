349f398b7ba251e5aa85db84f8e52da8d583418b
Sebastian Silva <sebastian@fuentelibre.org>
2025-02-26 12:09:43 -0500

    Setup for publication to pip

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="gtk-llm-chat",
-    version="1.0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         'PyGObject',
+        'markdown-it-py',
     ],
     entry_points={
         'console_scripts': [
